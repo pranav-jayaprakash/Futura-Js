efff106d91682d641b1b48c96194dab9d472afaf
Mona Lisa <pranavjayaprakashn98@gmail.com>
2023-07-19 10:23:10 +0530

    Task2 complete

--- Tasks/scriptss.js
+++ Tasks/scriptss.js
@@ -1,11 +1,21 @@
 // Program to swap two numbers without 3rd variable
 
-var a = 5
-var b = 9
+// var a = 5
+// var b = 9
 
-  a = a + b;
-  b = a - b;
-  a = a - b;
-console.log(a);
-console.log(b)
+//   a = a + b;
+//   b = a - b;
+//   a = a - b;
+// console.log(a);
+// console.log(b)
+
+
+// Program to find the area of a square
+
+// var a = prompt('Enter the side: ');
+
+// var area = Number(a)*Number(a)
+
+// console.log(area)
+
 
