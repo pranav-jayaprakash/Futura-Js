35e2c39d7c6e776bc3b2d34b4de602326a68a746
Mona Lisa <pranavjayaprakashn98@gmail.com>
2023-07-19 16:02:52 +0530

    Task complete

--- Tasks/scriptss.js
+++ Tasks/scriptss.js
@@ -1,4 +1,4 @@
-// Program to swap two numbers without 3rd variable
+//1. Program to swap two numbers without 3rd variable
 
 // var a = 5
 // var b = 9
@@ -10,7 +10,7 @@
 // console.log(b)
 
 
-// Program to find the area of a square
+//2. Program to find the area of a square
 
 // var a = prompt('Enter the side: ');
 
@@ -19,7 +19,7 @@
 // console.log(area)
 
 
-// Progarm to find the area of the rectangle
+//3. Progarm to find the area of the rectangle
 
 // var a = prompt('Enter the length: ');
 // var b = prompt('Enter the breadth: ');
@@ -27,3 +27,52 @@
 // var area = Number(a)*Number(b)
 
 // console.log(area)
+
+//4. Check if given number is prime or not 
+
+
+
+// function isPrime(number) {
+//     if (number <= 1) return false; 
+  
+//     for (let i = 2; i <= Math.sqrt(number); i++) {
+//       if (number % i === 0) return false; 
+//     }
+  
+//     return true; 
+//   }
+  
+//   var a = Number(prompt('Enter the number: '));
+
+//   if (isPrime(a)) {
+//     console.log(`${a} is a prime number.`);
+//   } else {
+//     console.log(`${a} is not a prime number.`);
+//   }
+
+
+
+  // 5. Check if the given number is odd or even.
+
+
+//   var a = Number(prompt('Enter the number: '));
+
+//   if (a%2===0) {
+//     console.log("Even Number")
+//   }
+//   else {
+//     console.log("Odd number")
+//   }
+
+// 6. Check if the given string is a palindrome or not.
+
+// var a = prompt('Enter the string: ');
+
+// if (a===a.split('').reverse().join('')){
+//     console.log("Palindrome")
+// }
+// else {
+//     console.log("Not a Palindrome")
+// }
+
+
